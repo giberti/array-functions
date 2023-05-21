28c11438d8b240d9397204cfa90fb3063e22da58
Erik Giberti <erik@af-design.com>
2023-05-21 07:48:44 -0500

    Fix operator

--- tests/ArrayRangeTest.php
+++ tests/ArrayRangeTest.php
@@ -21,7 +21,7 @@ class ArrayRangeTest extends \PHPUnit\Framework\TestCase
     public function test_array_range($data, $expected)
     {
         $range = array_range($data);
-        if (version_compare(\PHPUnit\Runner\Version::id(), '8.0.0', 'gte')) {
+        if (version_compare(\PHPUnit\Runner\Version::id(), '8.0.0', '>=')) {
              $this->assertEqualsWithDelta($expected, $range, 0.01);
         } else {
             $this->assertEquals($expected, $range, '', 0.01);
